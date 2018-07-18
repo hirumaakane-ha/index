057729ae5ca129047175e40bdf761932e294430d
Fugunagi <hoanganh.theodore@icloud.com>
2018-07-17 22:15:45 -0400

    scripts

---
+++ css/scripts.js
@@ -0,0 +1,11 @@
+if (screen.width >= 1920) {
+    document.write("<style>body {transform: scale(1.0)}</style>");
+}
+
+if (screen.width >= 1366) {
+    document.write("<style>body {transform: scale(9.0)}</style>");
+}
+
+if (screen.width >= 1280) {
+    document.write("<style>body {transform: scale(8.0)}</style>");
+}
